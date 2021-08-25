017710a3329b3e4992ed3372624880ed4ba5516d
Lee-Duckwon <a09519@naver.com>
2021-08-25 20:25:15 +0900

    [Client] / #12 / create: challengeComponent

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./App.css";
-import Main from "./features/main";
-
+import Main from "./components/Main";
+import Challenge from "./components/Challenge";
 function App() {
   return (
     <div className="App">
       <Main></Main>
+      <Challenge></Challenge>
     </div>
   );
 }
