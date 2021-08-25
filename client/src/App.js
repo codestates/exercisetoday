32e3cfe54d89c05f2ce3a5baf45a6647fdcb3341
Lee-Duckwon <a09519@naver.com>
2021-08-25 14:41:06 +0900

    [Client] / #12 / create : main.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import Main from "./features/main";
 
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <div className="App">
+      <Main></Main>
+    </div>
+  );
 }
 
 export default App;
