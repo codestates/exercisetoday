0ca150f0c55020b3cbde97ee2af102f8c21c0d03
KimMinchan95 <hialscks@gmail.com>
2021-08-27 01:47:28 +0900

    [Client] / #30 / create: SignUpPage

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,27 @@
 import React from "react";
 import "./App.css";
-import MainPage from "./components/MainPage";
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import MainPage from "./components/MainPage";
+import SignUpPage from "./components/SignUpPage";
+
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <MainPage></MainPage>
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <MainPage />
+          </Route>
+          <Route path="/signup">
+            <SignUpPage />
+          </Route>
+        </Switch>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
