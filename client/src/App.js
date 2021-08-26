0496fcfeb7bfd73f9f5ed358dba11dc3b8ca49fe
Lee-Duckwon <a09519@naver.com>
2021-08-26 11:41:34 +0900

    [Client] / #20 / create: Footer.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 import MainPage from "./components/MainPage";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 function App() {
   return (
     <div className="App">
       <Header />
-      <MainPage></MainPage>
+      <MainPage />
+      <Footer />
     </div>
   );
 }
