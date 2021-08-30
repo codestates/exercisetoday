753c3bf454272f6ec32d769be40c4f3d70bcdf8a
Sung Kang <79678662+spirited-hunger@users.noreply.github.com>
2021-08-30 22:53:44 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 cd /home/ubuntu/exercisetoday/server
-
-export ACCESS_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names ACCESS_SECRET --query Parameters[0].Value | sed 's/"//g')
-export KAKAO_CLIENT_ID=$(aws ssm get-parameters --region ap-northeast-2 --names KAKAO_CLIENT_ID --query Parameters[0].Value | sed 's/"//g')
-export KAKAO_CLIENT_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names KAKAO_CLIENT_SECRET --query Parameters[0].Value | sed 's/"//g')
-
 authbind --deep pm2 start app.js
