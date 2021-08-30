cead8fb38233988131423c3e70911aefe4acebf6
Sung Kang <79678662+spirited-hunger@users.noreply.github.com>
2021-08-30 22:40:14 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-cd /home/ubuntu/exercisetoday/server
+cd /home/ubuntu/im-sprint-practice-deploy/server
+
+export ACCESS_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names ACCESS_SECRET --query Parameters[0].Value | sed 's/"//g')
+export KAKAO_CLIENT_ID=$(aws ssm get-parameters --region ap-northeast-2 --names KAKAO_CLIENT_ID --query Parameters[0].Value | sed 's/"//g')
+export ACCESS_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names ACCESS_SECRET --query Parameters[0].Value | sed 's/"//g')
+
 authbind --deep pm2 start app.js
