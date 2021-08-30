fce3a688cc5e0d0fce4311f7af2cc0931d5928d4
Spirited Hunger <sjkang920921@gmail.com>
2021-08-30 10:56:54 +0900

    [Server] / #55 / test:root route

--- server/routes/root.js
+++ server/routes/root.js
@@ -3,8 +3,27 @@ const router = express.Router();
 
 
 // 테스트용 ROOT 라우트
-router.get('/', (req, res) => {
-  res.send('HI EXERCISE TODAY SERVER!')
+router.post('/', (req, res) => {
+
+  const reqbodydata = req.body.data;
+
+  res.status(200).json({
+    data: {
+      "user_id": 1,
+      "user_email": "myemail@gmail.com",
+      "user_password": "123456",
+      "user_name": "오하운",
+      "user_nickname": "오계란",
+      "user_gender": "male",
+      "user_mobile": 01011111111,
+      "user_exp": 42,
+      "user_photo": null,
+      "createdAt": new Date(),
+      "updatedAt": new Date(),
+      "reqbody": reqbodydata     
+    },
+    message: 'TEST ok, server connected!'
+  })
 });
 
 module.exports = router;
