73609bb2ddd2e15d3793965cd85b198de5738f4f
Sung Kang <79678662+spirited-hunger@users.noreply.github.com>
2021-09-01 00:33:02 +0900

    Update signout.js

--- server/controllers/user/signout.js
+++ server/controllers/user/signout.js
@@ -2,12 +2,18 @@ module.exports = {
   post: (req, res) => { 
 
     const token = req.headers.authorization;
-    const origin = "";
     
     if(token.split(" ")[0] === "jwt") {
       res.status(200).json({
         data : null,
-        message : 'Signed out successfully'
+        message : 'jwt signed out successfully'
+      })
+    }
+    
+    if(token.split(" ")[0] === "kakao") {
+      res.status(200).json({
+        data : null,
+        message : 'kakao signed out successfully'
       })
     }
 
