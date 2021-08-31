d873e7cefd944c1c889b48ad0c216d3970fc3d8c
Spirited Hunger <sjkang920921@gmail.com>
2021-08-31 21:10:36 +0900

    [Server] / #89 / feature:express communication

--- server/controllers/user/signout.js
+++ server/controllers/user/signout.js
@@ -1,1 +1,21 @@
-// 상국
+module.exports = {
+  post: (req, res) => { 
+
+    const kakao = req.cookies.kakao;
+    const jwt = req.cookies.jwt;
+
+    if(kakao) {
+      res.clearCookie('jwt');
+    }
+    if(jwt) {
+      res.clearCookie('jwt');
+    }
+
+    res.json({
+      data : null,
+      message : 'Signed out successfully'
+    })
+
+
+  }
+}
